fccf5088e6ef6deb4a570d459b87da34519f82f7
Wes <46760877+wwesley294@users.noreply.github.com>
2020-04-20 11:59:33 -0500

    Update process_data.py

--- process_data.py
+++ process_data.py
@@ -3,77 +3,57 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 
+'''
+process_data.py is a data ETL pipeline that prepares the message and category data for the machine learning pipeline.
+
+'''
+
+# load_data takes two arguments to file paths to create a new DataFrame as a single source of data.
 def load_data(messages_filepath, categories_filepath):
-    # load messages dataset
+
+    # Load messages and categories from CSVs into DataFrames.
     df_messages = pd.read_csv(messages_filepath)
-    # load categories dataset
     df_categories = pd.read_csv(categories_filepath)
 
-    # create a dataframe of the 36 individual category columns
+    # Split the categories at ';' and expand them into 36 individual columns.
     df_categories = df_categories['categories'].str.split(';', expand=True)
-    # select the first row of the categories dataframe
+
+    # Reconstruction column name and apply to the new DataFrame.
     row = df_categories.iloc[0]
-    # extract a list of new column names for categories.
     category_colnames = [item[:-2] for item in row]
-    # rename the columns of `categories`
     df_categories.columns = category_colnames
 
-    # set each value to be the last character of the string
+    # Convert category columns into binary data of integers 0 and 1.
     for col in df_categories.columns:
         df_categories[col] = df_categories[col].str[-1]
-        # convert column from string to numeric
         df_categories[col] = df_categories[col].astype(int)
 
-    # drop the original categories column from `df`
-    # df_categories = df_categories.drop(columns='categories')
-    # concatenate the original dataframe with the new `categories` dataframe
+    # Concatenate the two DateFrames.
     df = pd.concat([df_messages, df_categories], axis=1)
 
     return df
 
 
+# clean_data takes an argument of the new DataFrame and further removed unnecessary data.
 def clean_data(df):
+
+    # Remove rows with duplicate message and null values. Also drop the "id" and "original" columns.
     df = df.drop_duplicates(subset=['message'], keep=False)
-    df = df.drop(columns=['id', 'original', 'genre'])
+    df = df.drop(columns=['id', 'original'])
     df = df.dropna(axis=0, how='any')
+
     return df
 
 
+# save_data deposits the new DataFrame to a database.
 def save_data(df, database_filepath):
+
+    # Create a SQLite database and save the DataFrame as table "comm".
     engine = create_engine('sqlite:///' + database_filepath)
     df.to_sql('comm', engine, index=False)
 
 
-'''
-
-def main():
-    if len(sys.argv) == 4:
-
-        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
-
-        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
-              .format(messages_filepath, categories_filepath))
-        df = load_data(messages_filepath, categories_filepath)
-
-        print('Cleaning data...')
-        df = clean_data(df)
-
-        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
-        save_data(df, database_filepath)
-
-        print('Cleaned data saved to database!')
-
-    else:
-        print('Please provide the filepaths of the messages and categories '\
-              'datasets as the first and second argument respectively, as '\
-              'well as the filepath of the database to save the cleaned data '\
-              'to as the third argument. \n\nExample: python process_data.py '\
-              'disaster_messages.csv disaster_categories.csv '\
-              'DisasterResponse.db')
-
-'''
-
-
+# main executes the functions above in order while providing progress updates.
 def main():
     messages_filepath = 'disaster_messages.csv'
     categories_filepath = 'disaster_categories.csv'
@@ -91,5 +71,6 @@ def main():
     print('Cleaned data saved to database!')
 
 
+# Execute main.
 if __name__ == '__main__':
     main()
